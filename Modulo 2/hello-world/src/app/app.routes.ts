f40d2479c144d30cdfc981acf09cef60f36ba995
guilhermeDTNA <gleite98@gmail.com>
2024-03-20 22:20:31 -0300

    Estudando rotas e componentes

--- Modulo 2/hello-world/src/app/app.routes.ts
+++ Modulo 2/hello-world/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ListarComponent } from './pages/listar/listar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/home', 
+        pathMatch: 'full'
+    },
+
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+
+    {
+        path: 'listar',
+        component: ListarComponent
+    }
+];
---
+++ Modulo 2/hello-world/src/app/pages/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+
+export class HomeComponent implements OnInit {
+  constructor(){}
+
+  nome = "Guilherme"
+  ngOnInit(): void {
+    
+  }
+}
