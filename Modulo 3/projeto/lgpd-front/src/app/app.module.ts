9c5408ae379324786388f9967205dbdfcf656bc2
guilhermeDTNA <gleite98@gmail.com>
2024-05-18 16:13:26 -0300

    Corrigindo erros do ng build

---
+++ Modulo 3/projeto/lgpd-front/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SharedModule } from "./shared/shared.module";
+import { CourseListComponent } from './pages/course/course-list/course-list.component';
+import { CourseFormComponent } from './pages/course/course-form/course-form.component';
+import { EvaluationListComponent } from './pages/evaluation/evaluation-list/evaluation-list.component';
+import { EvaluationFormComponent } from './pages/evaluation/evaluation-form/evaluation-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseListComponent,
+    CourseFormComponent,
+    EvaluationListComponent,
+    EvaluationFormComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Modulo 3/projeto/lgpd-front/src/app/pages/evaluation/evaluation.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EvaluationRoutingModule } from './evaluation-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { EvaluationListComponent } from './evaluation-list/evaluation-list.component';
+import { EvaluationFormComponent } from './evaluation-form/evaluation-form.component';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    EvaluationRoutingModule,
+    SharedModule
+  ]
+})
+export class EvaluationModule { }
