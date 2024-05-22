20b0aefcaae7d6db780b9b03bc9af2ec02c75bb9
guilhermeDTNA <gleite98@gmail.com>
2024-05-21 22:52:35 -0300

    Armazenando imagem do usuário no back-end

--- Modulo 3/projeto/lgpd-front/src/app/app.module.ts
+++ Modulo 3/projeto/lgpd-front/src/app/app.module.ts
@@ -5,10 +5,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from "./shared/shared.module";
-import { CourseListComponent } from './pages/course/course-list/course-list.component';
-import { CourseFormComponent } from './pages/course/course-form/course-form.component';
-import { EvaluationListComponent } from './pages/evaluation/evaluation-list/evaluation-list.component';
-import { EvaluationFormComponent } from './pages/evaluation/evaluation-form/evaluation-form.component';
 
 @NgModule({
   declarations: [
